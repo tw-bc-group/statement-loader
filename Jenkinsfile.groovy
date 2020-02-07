4fe1b4aebec1c54800b3b469800f1a51e0ba47f7
Jin <jbjxu@thoughtworks.com>
2020-02-07 11:15:41 +0800

    chore: update Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,32 +1,19 @@
 pipeline {
-  agent none
+  agent any
   stages {
     stage('Test') {
-      agent {
-        docker {
-          image 'gradle:6.0.1-jdk8'
-          args '-v /tmp/gradle-caches:/home/gradle/.gradle/caches'
-        }
-      }
       steps {
         sh './gradlew test'
       }
     }
 
     stage('Build') {
-      agent {
-        docker {
-          image 'gradle:6.0.1-jdk8'
-          args '-v /tmp/gradle-caches:/home/gradle/.gradle/caches'
-        }
-      }
       steps {
         sh './gradlew build'
       }
     }
 
     stage('Dockerize') {
-      agent any
       steps {
         sh 'docker build -t tw-blockchain/reconciliation-loader .'
         sh 'docker image tag tw-blockchain/reconciliation-loader localhost:5000/reconciliation-loader'
